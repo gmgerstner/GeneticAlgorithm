55bb6b17b7005cd0bf5b03b43367a69885bea1c2
George M. Gerstner <gmgerstner@gmail.com>
2023-04-08 23:38:33 -0500

    Fitness functions

--- GeneticAlgorithm.MainApp/Creature.cs
+++ GeneticAlgorithm.MainApp/Creature.cs
@@ -12,9 +12,32 @@
             chromosone = GenerateChromosome();
         }
 
-        public int CalculateFitness()
+        //// Fitness function: determine how well a chromosome solves the problem
+        //// multiple criteria
+        //public int EvaluateFitness(string chromosome)
+        //{
+        //    int numOnes = chromosome.Count(c => c == '1');
+        //    int numZeroes = chromosome.Count(c => c == '0');
+        //    int numRepeats = chromosome.Length - chromosome.Distinct().Count();
+
+        //    // Calculate fitness score as a weighted sum of the criteria
+        //    double fitnessScore = 0.5 * numOnes - 0.2 * numZeroes - 0.3 * numRepeats;
+        //    return (int)Math.Round(fitnessScore);
+        //}
+
+        // Fitness function: determine how well a chromosome solves the problem
+        // single criteria
+        static int Fitness(string chromosome)
         {
-            return 0;
+            int score = 0;
+            for (int i = 0; i < chromosomeLength; i++)
+            {
+                if (chromosome[i] == targetString[i])
+                {
+                    score++;
+                }
+            }
+            return score;
         }
 
         // Generate a random chromosome
