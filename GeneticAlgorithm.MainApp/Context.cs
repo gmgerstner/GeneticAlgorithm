6110d8f8804eb9b3a5fc33a455940fc0b5d1e8ae
George M. Gerstner <gmgerstner@gmail.com>
2023-04-08 21:15:24 -0500

    Initial code

---
+++ GeneticAlgorithm.MainApp/Context.cs
@@ -0,0 +1,34 @@
+﻿namespace GeneticAlgorithm.MainApp
+{
+    internal class Context
+    {
+        private readonly int populationSize;
+        private readonly int numberOfGenerations;
+        private readonly int mutationRate;
+
+
+        public Context(int populationSize, int numberOfGenerations, int mutationRate)
+        {
+            // Get parameters: population size, # of generations, mutation rate
+            this.populationSize = populationSize;
+            this.numberOfGenerations = numberOfGenerations;
+            this.mutationRate = mutationRate;
+
+            Console.WriteLine("Parameters initialized.");
+        }
+
+        internal void RunSimulation()
+        {
+            Console.WriteLine("Simulation started.");
+
+            // 2) Initialize population of successors randomly
+            // 3) Evaulate fitness of each successors in the population
+            // 4) Select parents for reporduction based on the fitness (higher fitness successors are likely more chosen)
+            // 5) Apply crossover/mutation to selected parents to create successors (offspring)
+            // 6) Same as step 3
+            // 7) Select top N candidates w/ highest fitness
+            // 8) Check if max # of generations reached, solution found, fitness threshold achieved
+            // 9) If criterion ws not met, reselect parents (step 4)
+        }
+    }
+}
--- GeneticAlgorithm.MainApp/Program.cs
+++ GeneticAlgorithm.MainApp/Program.cs
@@ -1,7 +1,25 @@
-﻿internal class Program
+﻿using GeneticAlgorithm.MainApp;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string? inputText;
+        Console.WriteLine("Enter:");
+
+        Console.Write("> Population Size = ");
+        inputText = Console.ReadLine();
+        int populationSize = Convert.ToInt32(inputText);
+
+        Console.Write("> Number of Generations = ");
+        inputText = Console.ReadLine();
+        int numberOfGenerations = Convert.ToInt32(inputText);
+
+        Console.Write("> Mutation Rate = ");
+        inputText = Console.ReadLine();
+        int mutationRate = Convert.ToInt32(inputText);
+
+        Context context = new Context(populationSize, numberOfGenerations, mutationRate);
+        context.RunSimulation();
     }
 }
